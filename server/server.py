b0a645674681fdf921239baea41895bd01f6877d
Dantara <supercreed2@gmail.com>
2020-09-17 16:05:56 +0300

    Multi threads

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ import tqdm
 import os
 import operator
 from functools import reduce
-
+from threading import Thread
 
 # device's IP address
 SERVER_HOST = "0.0.0.0"
@@ -15,85 +15,90 @@ SEPARATOR = "<SEPARATOR>"
 
 RECEIVE_FOLDER = "received"
 
-# create the server socket
-# TCP socket
-s = socket.socket()
+class ClientListener(Thread):
+    def __init__(self, sock, addr):
+        super().__init__(daemon=True)
+        self.sock = sock
+        self.addr = addr
 
-# bind the socket to our local address
-s.bind((SERVER_HOST, SERVER_PORT))
+    def _is_file_exist(self, filename):
+        return os.path.isfile(f"{RECEIVE_FOLDER}/{filename}")
 
-# enabling our server to accept connections
-# 5 here is the number of unaccepted connections that
-# the system will allow before refusing new connections
-s.listen(5)
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+    def _add_copy_suffix(self, filename, n):
+        components = filename.split(".")
 
-def is_file_exist(filename):
-    return os.path.isfile(f"{RECEIVE_FOLDER}/{filename}")
+        if(len(components) == 1):
+            return f"{filename}_copy{n}"
 
-def add_copy_suffix(filename, n):
-    components = filename.split(".")
+        head, *tail = components
 
-    if(len(components) == 1):
-        return f"{filename}_copy{n}"
+        tail = ".".join(tail)
 
-    head, *tail = components
+        return f"{head}_copy{n}.{tail}"
 
-    tail = reduce(operator.add, tail)
+    def _get_valid_filename(self, filename):
+        if(not self._is_file_exist(filename)):
+            return filename
 
-    return f"{head}_copy{n}.{tail}"
+        n = 1
 
-def get_valid_filename(filename):
-    if(not is_file_exist(filename)):
-        return filename
+        while(self._is_file_exist(self._add_copy_suffix(filename, n))):
+            n += 1
 
-    n = 1
+        return self._add_copy_suffix(filename, n)
 
-    while(is_file_exist(add_copy_suffix(filename, n))):
-        n += 1
+    def run(self):
+        received = self.sock.recv(BUFFER_SIZE).decode()
+        filename, filesize = received.split(SEPARATOR)
+        # remove absolute path if there is
+        filename = os.path.basename(filename)
+        filename = self._get_valid_filename(filename)
 
-    return add_copy_suffix(filename, n)
+        file_path = f"{RECEIVE_FOLDER}/{filename}"
+
+        # convert to integer
+        filesize = int(filesize)
+
+        # start receiving the file from the socket
+        # and writing to the file stream
+        progress = tqdm.tqdm(range(filesize), f"[*] Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024, leave=False)
+        with open(file_path, "wb") as f:
+            for _ in progress:
+                # read 1024 bytes from the socket (receive)
+                bytes_read = self.sock.recv(BUFFER_SIZE)
+                if not bytes_read:
+                    # nothing is received
+                    # file transmitting is done
+                    break
+                # write to the file the bytes we just received
+                f.write(bytes_read)
+                # update the progress bar
+                progress.update(len(bytes_read))
+
+            print(f"[+] File received: {filename}")
+            self._close()
+
+    def _close(self):
+        self.sock.close()
+        print(f"[+] {self.addr[0]}:{self.addr[1]} is disconnected.")
 
 
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-while(True):
-    # accept connection if there is any
-    client_socket, address = s.accept()
-    # if below code is executed, that means the sender is connected
-    print(f"[+] {address} is connected.")
+    sock.bind((SERVER_HOST, SERVER_PORT))
+    sock.listen()
 
-    # receive the file infos
-    # receive using client socket, not server socket
-    received = client_socket.recv(BUFFER_SIZE).decode()
-    filename, filesize = received.split(SEPARATOR)
-    # remove absolute path if there is
-    filename = os.path.basename(filename)
-    filename = get_valid_filename(filename)
+    print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
-    file_path = f"{RECEIVE_FOLDER}/{filename}"
+    while True:
+        con, addr = sock.accept()
 
-    # convert to integer
-    filesize = int(filesize)
+        print(f"[+] {addr[0]}:{addr[1]} is connected.")
 
-    # start receiving the file from the socket
-    # and writing to the file stream
-    progress = tqdm.tqdm(range(filesize), f"[*] Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024, leave=False)
-    with open(file_path, "wb") as f:
-        for _ in progress:
-            # read 1024 bytes from the socket (receive)
-            bytes_read = client_socket.recv(BUFFER_SIZE)
-            if not bytes_read:
-                # nothing is received
-                # file transmitting is done
-                break
-            # write to the file the bytes we just received
-            f.write(bytes_read)
-            # update the progress bar
-            progress.update(len(bytes_read))
+        ClientListener(con, addr).start()
 
-    # close the client socket
-    client_socket.close()
-    print(f"[+] File received: {filename}")
 
-# close the server socket
-s.close()
+if __name__ == "__main__":
+    main()
