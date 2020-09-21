fb0600bf256600ae235fdfd35562aa1ee119cee7
Dantara <supercreed2@gmail.com>
2020-09-21 19:07:39 +0300

    Commenting

--- client/client.py
+++ client/client.py
@@ -7,6 +7,7 @@ SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096
 
 def main(argv):
+    # Handling the wrong number of arguments
     if len(argv) != 3:
         print("You are specifed wrong number of arguments.")
         print("You should launch this app in the following way:")
@@ -14,28 +15,38 @@ def main(argv):
         print("Here is an example:")
         print("python client.py test.file 127.0.0.1 5000")
     else:
+        # Parsing command line options
         filename = argv[0]
         host = argv[1]
         port = int(argv[2])
 
+        # Get file size
         filesize = os.path.getsize(filename)
         s = socket.socket()
 
+        # Connecting to remote host
         print(f"[+] Connecting to {host}:{port}")
         s.connect((host, port))
         print("[+] Connected.")
 
+        # Seneing file name and file size
         s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
+        # Creating progress bar
         progress = tqdm.tqdm(range(filesize), f"[*] Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024, leave=False)
+
         with open(filename, "rb") as f:
             for _ in progress:
+                # Read the file
                 bytes_read = f.read(BUFFER_SIZE)
 
+                # Stop if full file was read
                 if not bytes_read:
                     break
 
+                # Send buffer data
                 s.sendall(bytes_read)
+                # Update progress bar state
                 progress.update(len(bytes_read))
 
             print(f"[+] File {filename} was sent.")
--- server/server.py
+++ server/server.py
@@ -4,12 +4,16 @@ import os
 import operator
 from threading import Thread
 
+# Server host name
 SERVER_HOST = "0.0.0.0"
+
+# Default port (can be cahnged via command line option)
 DEFAULT_PORT = 4000
 
 BUFFER_SIZE = 4096
 SEPARATOR = "<SEPARATOR>"
 
+# Separate folder to store recived files
 RECEIVE_FOLDER = "received"
 
 class ClientListener(Thread):
@@ -18,9 +22,11 @@ class ClientListener(Thread):
         self.sock = sock
         self.addr = addr
 
+    # Checking whether file exist or not
     def _is_file_exist(self, filename):
         return os.path.isfile(f"{RECEIVE_FOLDER}/{filename}")
 
+    # Add copy suffix
     def _add_copy_suffix(self, filename, n):
         components = filename.split(".")
 
@@ -33,6 +39,7 @@ class ClientListener(Thread):
 
         return f"{head}_copy{n}.{tail}"
 
+    # Add copy suffix if needed
     def _get_valid_filename(self, filename):
         if(not self._is_file_exist(filename)):
             return filename
@@ -45,38 +52,48 @@ class ClientListener(Thread):
         return self._add_copy_suffix(filename, n)
 
     def run(self):
+        # Get file name
         received = self.sock.recv(BUFFER_SIZE).decode()
         filename, filesize = received.split(SEPARATOR)
         filename = os.path.basename(filename)
         filename = self._get_valid_filename(filename)
 
+        # Create path to file
         file_path = f"{RECEIVE_FOLDER}/{filename}"
 
         filesize = int(filesize)
 
+        # Recieve the file
         with open(file_path, "wb") as f:
             while(True):
+                # Read from socket
                 bytes_read = self.sock.recv(BUFFER_SIZE)
 
+                # Stop if file fully recieved
                 if not bytes_read:
                     break
 
+                # Write to file
                 f.write(bytes_read)
 
             print(f"[+] File received: {filename}")
             self._close()
 
     def _close(self):
+        # Close connection
         self.sock.close()
         print(f"[+] {self.addr[0]}:{self.addr[1]} is disconnected.")
 
 
 def main(argv):
+    # Change default port if needed
     SERVER_PORT = int(argv[0]) if len(argv) == 1 else DEFAULT_PORT
 
+    # Setting socket options
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+    # Bind socket
     sock.bind((SERVER_HOST, SERVER_PORT))
     sock.listen()
 
@@ -87,6 +104,7 @@ def main(argv):
 
         print(f"[+] {addr[0]}:{addr[1]} is connected.")
 
+        # Start new thread for new user
         ClientListener(con, addr).start()
 
 
